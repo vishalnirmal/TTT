4227e5c18fa872c483661b095428a04741c749b7
vishalnirmal <vishalnirmal53@gmail.com>
2017-02-11 16:06:36 +0530

    Add files via upload

---
+++ ttt.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import javax.swing.JApplet;
+
+/**
+ *
+ * @author vishalnirmal
+ */
+public class ttt extends JApplet {
+
+    /**
+     * Initialization method that will be called after the applet is loaded into
+     * the browser.
+     */
+    public void init() {
+        // TODO start asynchronous download of heavy resources
+    }
+    // TODO overwrite start(), stop() and destroy() methods
+}
